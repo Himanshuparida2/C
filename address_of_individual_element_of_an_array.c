9d6f4d1b97cb64156107905975e2fab0ad69da70
Himanshu Sekhar Parida <118821114+Himanshuparida2@users.noreply.github.com>
2024-10-23 16:14:59 +0530

    Delete address_of_individual_element_of_an_array.c

--- address_of_individual_element_of_an_array.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-int arr[4][2]={{12,1},{13,2},{14,3},{15,4}};
-for(int i=0;i<4;i++){
-      printf("The Address of %d element of the 1D Array : %ld \n",i,(long)arr[i]);
-      printf("The Address of 2nd element od the 1D Array : %ld\n",(long)(arr[i]+1));
-}
-return 0;
-}
